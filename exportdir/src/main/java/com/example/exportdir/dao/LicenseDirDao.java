49ef1c1e02aaddca9054b5f4e8753619088cc06b
Jeff Wang(王哲) <wangzhe@inspur.com>
2018-07-01 15:57:48 +0800

    update

--- exportdir/src/main/java/com/example/exportdir/dao/LicenseDirDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.exportdir.dao;
-
-import com.example.exportdir.po.LicenseDirBean;
-import com.example.springtemplate.db.BaseDao;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class LicenseDirDao extends BaseDao<LicenseDirBean> {
-}
