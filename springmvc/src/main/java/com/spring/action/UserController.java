7dbdd065028790bfdb89575ca3bae67453a910ef
Jeff Wang(王哲) <wangzhe@inspur.com>
2018-07-24 09:52:58 +0800

    add log4j

--- springmvc/src/main/java/com/spring/action/UserController.java
+++ springmvc/src/main/java/com/spring/action/UserController.java
@@ -2,8 +2,9 @@ package com.spring.action;
 
 import com.spring.po.UserBean;
 import com.spring.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.annotation.AccessType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,14 +13,17 @@ import java.util.List;
 
 @Controller
 public class UserController {
+    static Logger logger = LogManager.getLogger(UserController.class);
+
     @Autowired
     UserService userService;
 
     @RequestMapping(value="/index")
     public String index(HttpServletRequest request){
-        UserBean param = new UserBean();
+        /*UserBean param = new UserBean();
         param.setUserName("123");
-        List<UserBean> userList = userService.findByParam(param);
+        List<UserBean> userList = userService.findByParam(param);*/
+        logger.error("error");
         return "index";
     }
 }
