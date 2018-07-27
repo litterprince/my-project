441134743c9a49134314a2c8f91c7f9ffe072db3
Jeff Wang(王哲) <wangzhe@inspur.com>
2018-07-27 11:14:55 +0800

    add

--- springmvc/src/main/java/com/spring/service/UserService.java
+++ springmvc/src/main/java/com/spring/service/UserService.java
@@ -4,10 +4,12 @@ import com.spring.dao.UserDao;
 import com.spring.po.UserBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service("userService")
+@Transactional
 public class UserService {
     @Autowired
     UserDao userDao;
