ef87aef6b5d30b746fba43783d4067c4c962c91a
litterprince <444701246@qq.com>
2019-04-01 16:58:29 +0800

    update, finish rpc-netty module

--- springrpc/rpc-zk/src/main/java/com/spring/zk/client/RpcProxy.java
+++ springrpc/rpc-zk/src/main/java/com/spring/zk/client/RpcProxy.java
@@ -1,7 +1,17 @@
 package com.spring.zk.client;
 
+import com.spring.zk.coder.RpcDecoder;
+import com.spring.zk.coder.RpcEncoder;
 import com.spring.zk.message.RpcRequest;
 import com.spring.zk.message.RpcResponse;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -44,8 +54,31 @@ public class RpcProxy {
                         String host = array[0];
                         int port = Integer.parseInt(array[1]);
 
-                        RpcClient client = new RpcClient(host, port); // 初始化 RPC 客户端
-                        RpcResponse response = client.send(request); // 通过 RPC 客户端发送 RPC 请求并获取 RPC 响应
+                        // TODO: 每次方法调用都要启动一次netty，性能不高
+                        // start client netty
+                        EventLoopGroup group = new NioEventLoopGroup();
+                        ClientHandler handler = new ClientHandler();
+                        try {
+                            Bootstrap b = new Bootstrap();
+                            b.group(group).channel(NioSocketChannel.class).handler(new ChannelInitializer<SocketChannel>() {
+                                @Override
+                                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                                    socketChannel.pipeline().addLast(new RpcEncoder(RpcRequest.class));
+                                    socketChannel.pipeline().addLast(new RpcDecoder(RpcResponse.class));
+                                    socketChannel.pipeline().addLast(handler);
+                                }
+                            }).option(ChannelOption.SO_KEEPALIVE, true);
+
+                            ChannelFuture future = b.connect(host, port).sync();
+                            future.channel().writeAndFlush(request).sync();
+
+                            future.channel().closeFuture().sync();
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        } finally {
+                            group.shutdownGracefully();
+                        }
+                        RpcResponse response = handler.getRpcResponse();
 
                         if (response.getError() != null) {
                             throw response.getError();
