e9b32c11016b761e281f9758041ad2074cbaa074
wangzhe01 <wangzhe01@koolearn.com>
2019-07-12 11:49:05 +0800

    update

--- java/src/main/java/com/lambdas/StreamTest.java
+++ java/src/main/java/com/lambdas/StreamTest.java
@@ -1,6 +1,7 @@
 package com.lambdas;
 
 import lombok.Data;
+import scala.Int;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,27 +16,58 @@ public class StreamTest {
         initParam();
         beanListToIntList();
         beanListToMap();
+        testArray();
+        listToArray();
     }
 
-    private static void initParam(){
+    private static void initParam() {
         person.setId(1);
         person.setName("jeff");
         list.add(person);
     }
 
-    private static void beanListToIntList(){
+    private static void beanListToIntList() {
         List<Integer> collect = list.stream().map(Person::getId).collect(Collectors.toList());
         System.out.println(collect);
     }
 
-    private static void beanListToMap(){
+    private static void beanListToMap() {
         Map<Integer, Person> map = list.stream().collect(Collectors.toMap(Person::getId, Person -> Person));
         System.out.println(map);
     }
 
+    public static void testArray() {
+        List<Person> userProductDelayDTOList = new ArrayList<>();
+        Person a = new Person();
+        a.setId(11);
+        a.setStatus(1);
+        Person b = new Person();
+        b.setId(22);
+        b.setStatus(2);
+        Person c = new Person();
+        c.setId(33);
+        c.setStatus(3);
+        userProductDelayDTOList.add(a);
+        userProductDelayDTOList.add(b);
+        userProductDelayDTOList.add(c);
+        List<Integer> delayProductIds = userProductDelayDTOList.stream().filter(
+                Person-> !(Person.getStatus() == 2 || Person.getStatus() == 3)
+        ).map(Person::getId).collect(Collectors.toList());
+        System.out.println(delayProductIds);
+    }
+
+    public static void listToArray(){
+        List<Integer> collect = list.stream().map(Person::getId).collect(Collectors.toList());
+        int[] ints = collect.stream().mapToInt(Integer::intValue).toArray();
+        for (int i : ints){
+            System.out.println(i);
+        }
+    }
+
     @Data
-    private static class Person{
+    private static class Person {
         private Integer id;
         private String name;
+        private Integer status;
     }
 }
