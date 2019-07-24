f1de42c30ca55241456b53b9ab4176fb8834b133
wangzhe01 <wangzhe01@koolearn.com>
2019-07-24 21:28:19 +0800

    update

--- springmvc/src/main/resources/db/db.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users(
-  id varchar(50) primary key ,
-  user_name varchar(50),
-  password varchar(50),
-  create_time TIMESTAMP
-)
--- springrpc/rpc-common/src/main/java/com/spring/common/service/HelloService.java
+++
@@ -1,5 +0,0 @@
-package com.spring.common.service;
-
-public interface HelloService {
-    String sayHello(String name);
-}
